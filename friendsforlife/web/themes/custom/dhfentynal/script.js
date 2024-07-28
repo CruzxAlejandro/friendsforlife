1aceb71d634d1ca0ee72058015ae4dcf2fdf1cba
CruzxAlejandro <cruzlopez@Dis-the-Cruz-machine.local>
2024-07-28 02:58:06 -0700

    Troubleshooting toolkit pages

--- friendsforlife/web/themes/custom/dhfentynal/script.js
+++ friendsforlife/web/themes/custom/dhfentynal/script.js
@@ -20,48 +20,113 @@
 
   $(document).ready(function () {
     var currentUrl = window.location.href;
-    if (currentUrl.indexOf("es") !== -1) {
-      $(this)
-        .find(
-          ".field--name-field-preview-pdf a.file-download.file-download-pdf"
-        )
-        .html("Vista previa");
-    }
-    $(".page-node-9 .paragraph .field--name-field-description").hide();
-    $(
-      ".page-node-9 .paragraph--type--toolkit .field--name-field-tiles .field__item"
-    ).each(function () {
-      var desc = $(this)
-        .find(".paragraph .field--name-field-description")
-        .text();
-      if (desc != "") {
-        $(this).find("a.file-download:not(.file-download-pdf)").html(desc);
-      }
-      //console.log(desc);
-    });
 
-    $(".page-node-12 .paragraph .field--name-field-description").hide();
-    $(
-      ".page-node-12 .paragraph--type--toolkit .field--name-field-tiles .field__item"
-    ).each(function () {
-      var desc = $(this)
-        .find(".paragraph .field--name-field-description")
-        .text();
-      if (desc != "") {
-        $(this).find("a.file-download").html(desc);
+    // Check for "es" in URL and update text
+    if (currentUrl.indexOf("es") !== -1) {
+      var pdfLink = $(
+        ".field--name-field-preview-pdf a.file-download.file-download-pdf"
+      );
+      if (pdfLink.length) {
+        pdfLink.html("Vista previa");
+      } else {
+        console.log("PDF link not found");
       }
-    });
-    $(".page-node-13 .paragraph .field--name-field-description").hide();
-    $(
-      ".page-node-13 .paragraph--type--toolkit .field--name-field-tiles .field__item"
-    ).each(function () {
-      var desc = $(this)
-        .find(".paragraph .field--name-field-description")
-        .text();
-      if (desc != "") {
-        $(this).find("a.file-download").html(desc);
-      }
-    });
+    }
+
+    // Hide and update descriptions for node 9
+    if ($(".page-node-9").length) {
+      $(".page-node-9 .paragraph .field--name-field-description").hide();
+      $(
+        ".page-node-9 .paragraph--type--toolkit .field--name-field-tiles .field__item"
+      ).each(function () {
+        var desc = $(this)
+          .find(".paragraph .field--name-field-description")
+          .text();
+        if (desc) {
+          $(this).find("a.file-download:not(.file-download-pdf)").html(desc);
+        }
+        console.log("desc: " + desc);
+      });
+    } else {
+      console.log("Node 9 not found");
+    }
+
+    // Hide and update descriptions for node 12
+    if ($(".page-node-12").length) {
+      $(".page-node-12 .paragraph .field--name-field-description").hide();
+      $(
+        ".page-node-12 .paragraph--type--toolkit .field--name-field-tiles .field__item"
+      ).each(function () {
+        var desc = $(this)
+          .find(".paragraph .field--name-field-description")
+          .text();
+        if (desc) {
+          $(this).find("a.file-download").html(desc);
+        }
+      });
+    } else {
+      console.log("Node 12 not found");
+    }
+
+    // Hide and update descriptions for node 13
+    if ($(".page-node-13").length) {
+      $(".page-node-13 .paragraph .field--name-field-description").hide();
+      $(
+        ".page-node-13 .paragraph--type--toolkit .field--name-field-tiles .field__item"
+      ).each(function () {
+        var desc = $(this)
+          .find(".paragraph .field--name-field-description")
+          .text();
+        if (desc) {
+          $(this).find("a.file-download").html(desc);
+        }
+      });
+    } else {
+      console.log("Node 13 not found");
+    }
+    // var currentUrl = window.location.href;
+    // if (currentUrl.indexOf("es") !== -1) {
+    //   $(this)
+    //     .find(
+    //       ".field--name-field-preview-pdf a.file-download.file-download-pdf"
+    //     )
+    //     .html("Vista previa");
+    // }
+    // $(".page-node-9 .paragraph .field--name-field-description").hide();
+    // $(
+    //   ".page-node-9 .paragraph--type--toolkit .field--name-field-tiles .field__item"
+    // ).each(function () {
+    //   var desc = $(this)
+    //     .find(".paragraph .field--name-field-description")
+    //     .text();
+    //   if (desc != "") {
+    //     $(this).find("a.file-download:not(.file-download-pdf)").html(desc);
+    //   }
+    //   console.log("desc: " + desc);
+    // });
+
+    // $(".page-node-12 .paragraph .field--name-field-description").hide();
+    // $(
+    //   ".page-node-12 .paragraph--type--toolkit .field--name-field-tiles .field__item"
+    // ).each(function () {
+    //   var desc = $(this)
+    //     .find(".paragraph .field--name-field-description")
+    //     .text();
+    //   if (desc != "") {
+    //     $(this).find("a.file-download").html(desc);
+    //   }
+    // });
+    // $(".page-node-13 .paragraph .field--name-field-description").hide();
+    // $(
+    //   ".page-node-13 .paragraph--type--toolkit .field--name-field-tiles .field__item"
+    // ).each(function () {
+    //   var desc = $(this)
+    //     .find(".paragraph .field--name-field-description")
+    //     .text();
+    //   if (desc != "") {
+    //     $(this).find("a.file-download").html(desc);
+    //   }
+    // });
 
     $(".closepopup").click(function () {
       if (localStorage.getItem("popState") != "shown") {
